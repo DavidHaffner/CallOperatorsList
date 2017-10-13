502e07d3931ffa1c7e940cb5c13ca8a128bc1d59
dhaffner <dhaffner@DHAFFNER-10.merlin.cz>
2017-10-13 09:35:28 +0200

    metoda na opravu čísla vyvedena jako samostatná; tj. metody phoneNumberCheck a phoneNumberCorrection se volají ve svých kódech navzájem

--- src/calloperatorslist/Record.java
+++ src/calloperatorslist/Record.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
  * @author dhaffner
  */
 public class Record {
+
     /* jméno klienta */
     private String firstName = "";
     /* příjmení klienta */
@@ -22,12 +23,11 @@ public class Record {
     private String phoneNumber = "";
     /* případná poznámka */
     private String notice = "";
-    
-    
-    public Record () {
+
+    public Record() {
     }
-    
-    public Record (String firstName, String surname, String phoneNumber, String notice) {
+
+    public Record(String firstName, String surname, String phoneNumber, String notice) {
         this.firstName = firstName;
         this.surname = surname;
         this.phoneNumber = phoneNumber;
@@ -35,7 +35,6 @@ public class Record {
         // vložit metody na kontrolu telefonního čísla vytvořené níže
     }
 
-    
     /**
      * @return the firstName
      */
@@ -91,49 +90,46 @@ public class Record {
     public void setNotice(String notice) {
         this.notice = notice;
     }
-    
-    
-    /** metoda zkontroluje platnost tel. čísla a příp. umožní opravu */
-    public String phoneNumberCheck (String phoneNumber) {
+
+    /**
+     * metoda zkontroluje platnost tel. čísla a příp. umožní opravu
+     */
+    public String phoneNumberCheck(String phoneNumber) {
         String pNumber = phoneNumber.replaceAll("\\D", "");   // všechny nečíslice pryč
         System.out.println("Po vyhození nečíslic: " + pNumber); //pomocný práskač
-        
+
         if (pNumber.length() == 9) {     // při 9 číslicích přidá předvolbu ČR
             pNumber = "420" + pNumber;
         }
-        
+
         if (pNumber.length() != 12) {    // když nesedí délka, umožní opravu 
-            System.out.println("The phone number is wrong.");
-            System.out.println("Insert it once again, please: ");
-             
-            try {
-            Scanner sc = new Scanner (System.in); //umožní znovuzadat ...
-            pNumber = sc.nextLine();
-            }
-            catch (Exception e) {
-                System.out.println("Error when inserting ...");
-            }
-            pNumber = phoneNumberCheck(pNumber);        // ... a znovu zkontroluje
+            pNumber = phoneNumberCorrection();
         }
-            
+
         return pNumber;
     }
-    
-    /** metoda převede tel. číslo na požadovaný formát */
-    public String phoneNumberFormat (String phoneNumber) {
+
+    /**
+     * metoda převede tel. číslo na požadovaný formát
+     */
+    public String phoneNumberFormat(String phoneNumber) {
         char[] chars = phoneNumber.toCharArray();
         phoneNumber = "+" + chars[0] + chars[1] + chars[2] + " " + chars[3]
                 + chars[4] + chars[5] + " " + chars[6] + chars[7] + chars[8]
                 + " " + chars[9] + chars[10] + chars[11];
         return phoneNumber;
-    }    
-   
-    /** metoda vrátí název souboru pro zápis záznamu */
-    public String fNameDeclare() {
-        return this.getSurname() +"_"+ this.getFirstName() + ".TXT";
     }
-    
-    /** metoda zapíše do souboru */
+
+    /**
+     * metoda vrátí název souboru pro zápis záznamu
+     */
+    public String fNameDeclare() {
+        return this.getSurname() + "_" + this.getFirstName() + ".TXT";
+    }
+
+    /**
+     * metoda zapíše do souboru
+     */
     public void fileWrite(String fileName) {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
             bw.write(this.getFirstName());
@@ -147,5 +143,24 @@ public class Record {
         } catch (Exception e) {
             System.err.println("Error when trying to write into the file");
         }
+    }
+
+    /**
+     * metoda umožní opravu telef. čísla (např. když se vyhodnotí, že bylo
+     * špatně zadáno
+     */
+    public String phoneNumberCorrection() {
+        System.out.println("The phone number is wrong.");
+        System.out.println("Insert it once again, please: ");
+
+        try {
+            Scanner sc = new Scanner(System.in); //umožní znovuzadat ...
+            phoneNumber = sc.nextLine();
+        } catch (Exception e) {
+            System.out.println("Error when inserting ...");
+        }
+        phoneNumber = phoneNumberCheck(phoneNumber);        // ... a znovu zkontroluje
+        
+        return phoneNumber;
     }
 }
